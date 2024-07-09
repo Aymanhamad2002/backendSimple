6d2c59ec6b498928e83445231ef3a82c1c7c623b
Aymanhamad2002 <aymanhamad200@gmail.com>
2024-07-09 17:09:06 +0300

    implementthe routes that handle the get request for all notes

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Note = require('./models/note')
+
+app.get('/api/notes',(request,response)  => {
+  Note.find({}).then(result => {
+    console.log(result)
+    response.json(result)
+  })
+})
+
+
+app.listen(process.env.PORT,() => {
+  console.log(`Server is running on port : ${process.env.PORT}`)
+})
